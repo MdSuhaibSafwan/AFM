3bd3454d8b7d897db122ef1f48a57939354858b6
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2024-09-21 06:07:19 +0600

    update settings files

--- AFM/settings.py
+++ AFM/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 DEVELOPMENT_MODE = config('DEVELOPMENT_MODE')
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', ".vercel.app", ".vercel", ".app"]
 
 # Application definition
 
