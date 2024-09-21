f94f3b3efdd0a7651701bd2ba0fe4c23e4a593a6
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2024-09-21 06:22:29 +0600

    update build file

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,12 @@
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
+ECHO "BUILD START"
+
+python3.9 -m venv venv
+
+ECHO "created virtualenv"
+
+source venv/bin/activate
+
+ECHO "activated virtual environment"
+
+pip3 install -r requirements.txt
+python3.9 manage.py collectstatic --no-input
