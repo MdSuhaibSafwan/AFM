9e7cc4f25f524b97fcdbb8aaebe47b0607fda264
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2024-09-23 19:52:03 +0600

    update static files

--- staticfiles/assets/js/style.js
+++
@@ -1,9 +0,0 @@
-$('.sec-modal').click(function(){
-    target = $(this).data('target');
-    $('.sec-modal').removeClass('text-primary');
-    $('.sec-modal').removeClass('underline');
-    $(this).addClass('underline');
-    $(this).addClass('text-primary');
-    $('.sec-target').addClass('d-none');
-    $('#'+target).removeClass('d-none');
-})
