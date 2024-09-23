0f4825a8addaf609702cbf77b31bf5f5bc749733
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2024-09-24 03:07:53 +0600

    update requirements

--- AFM/urls.py
+++ AFM/urls.py
@@ -40,18 +40,18 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('sitemap.xml',  sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('', include('administration.urls', namespace='administration')),
-    path('application/', include('application.urls', namespace='application')),
+    # path('application/', include('application.urls', namespace='application')),
     path('feedback/', include('feedback.urls', namespace='feedback')),
     path('faqs/', include('faqs.urls', namespace='faqs')),
-    path('med-school-blogs/', include('blogs.urls', namespace='blogs')),
+    # path('med-school-blogs/', include('blogs.urls', namespace='blogs')),
     path('message/', include('messaging.urls', namespace='messaging')),
     # path('page/', include('page.urls', namespace='page')),
-    path('inbox/notifications/', include('notifications.urls', namespace='notifications')),
+    # path('inbox/notifications/', include('notifications.urls', namespace='notifications')),
     path('accounts/password_reset/', auth_views.PasswordResetView.as_view(
         html_email_template_name='registration/password_reset_email.html'
     )),
-    path('booking/', include('bookings.urls', namespace='booking')),
-    path('summernote/', include('django_summernote.urls')),
+    # path('booking/', include('bookings.urls', namespace='booking')),
+    # path('summernote/', include('django_summernote.urls')),
     path('payment/', include('payments.urls', namespace='payments')),
     # override the SignupView of django-allauth
     path("social-accounts/social/signup/", view=MySignupView.as_view()),
@@ -61,10 +61,10 @@ urlpatterns = [
     path('social-accounts/', include('allauth.urls')),
     # path("select2/", include("django_select2.urls")),
     # path('captcha/', include('captcha.urls')),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('cookies/', include('gdpr_cookie_consent.urls', namespace='cookie_consent')),
+    # path('ckeditor/', include('ckeditor_uploader.urls')),
+    # path('cookies/', include('gdpr_cookie_consent.urls', namespace='cookie_consent')),
     # url(r'^oauth/', include('social_django.urls', namespace='social')),
-    re_path(r'^webpush/', include('webpush.urls')),
+    # re_path(r'^webpush/', include('webpush.urls')),
     # # url(r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:], protected_serve, {'document_root': settings.MEDIA_ROOT}),
 
 ]
