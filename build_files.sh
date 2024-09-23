1b4e1cea4c4ccbe7635e2a1548029c4528f1d88b
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2024-09-24 01:53:14 +0600

    update

--- build_files.sh
+++ build_files.sh
@@ -9,6 +9,6 @@ source venv/bin/activate
 echo "activated virtual environment"
 
 pip3 install -r requirements.txt
-python3.9 manage.py collectstatic --no-input
+python3.9 manage.py collectstatic --noinput
 
 echo "Finished"
